796fa2e8ef6499022101cb8f5620d2d0c0d270fc
Draveness <stark.draven@gmail.com>
2015-05-28 15:04:48 +0800

    Add reset btn

--- Setting/ATPSettingPanelWindowController.m
+++ Setting/ATPSettingPanelWindowController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ATPSettingPanelWindowController.h"
+#import "ATPropertySetting.h"
 
 @interface ATPSettingPanelWindowController ()
+
+@property (weak) IBOutlet NSButton *btnUseNonatomic;
+@property (weak) IBOutlet NSButton *atomicityPrefix;
 
 @end
 
@@ -16,7 +20,23 @@
 
 - (void)windowDidLoad {
     [super windowDidLoad];
-    
+    self.btnUseNonatomic.state = (NSCellStateValue)[[ATPropertySetting defaultSetting] useNonatomic];
+    self.atomicityPrefix.state = (NSCellStateValue)[[ATPropertySetting defaultSetting] atomicityPrefix];
+}
+
+- (IBAction)btnUseNonatomicPressed:(NSButton *)sender {
+    [[ATPropertySetting defaultSetting] setUseNonatomic:sender.state];
+}
+
+- (IBAction)btnAtomicityPrefixPressed:(NSButton *)sender {
+    [[ATPropertySetting defaultSetting] setAtomicityPrefix:sender.state];
+}
+
+- (IBAction)btnResetPressed:(NSButton *)sender {
+    [[ATPropertySetting defaultSetting] setUseNonatomic:YES];
+    [[ATPropertySetting defaultSetting] setAtomicityPrefix:YES];
+    self.btnUseNonatomic.state = (NSCellStateValue)[[ATPropertySetting defaultSetting] useNonatomic];
+    self.atomicityPrefix.state = (NSCellStateValue)[[ATPropertySetting defaultSetting] atomicityPrefix];
 }
 
 @end
